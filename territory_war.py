c8936ad7c33f1f90d0ab1e3b5b26f70cf2c36280
fferrari <fferrari@despegar.com>
2019-08-07 18:14:20 -0300

    print output in a more elegant way :)

--- territory_war.py
+++ territory_war.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import urllib3
+from prettytable import PrettyTable
 
 GUILD_URL = "https://swgoh.gg/api/guild/{}" # EDDP 8574 DO 52542
 INTEREST_TOONS = ["DARTHMALAK","DARTHREVAN","JEDIKNIGHTREVAN","DARTHTRAYA","PADMEAMIDALA","GEONOSIANBROODALPHA","GRIEVOUS"]
@@ -21,6 +22,14 @@ def count_interest_toon(toons_map, toon_id, gear, zetas):
         toons_map[key] = 1
     return toons_map
 
+def make_pretty_table(names, quantity, gear, zetas):
+    table = PrettyTable()
+    table.add_column("Unit", names)
+    table.add_column("Quantity", quantity)
+    table.add_column("Gear", gear)
+    table.add_column("Zetas", zetas)
+    return table
+
 def process(guild_id):
 
     response = get_guild_data(guild_id)
@@ -40,10 +49,31 @@ def process(guild_id):
     print("======")
     print(guild_name)
     print("======")
+
+    name_list = []
+    quantity_list = []
+    gear_list = []
+    zetas_list = []
+    last_name = None
     for k in sorted(toons_map.keys()):
 
         splitted_data = k.split("#")
-        print("{} {} Gear {} con {} zetas".format(toons_map[k],splitted_data[0],splitted_data[1],splitted_data[2]))
+
+        t_name = splitted_data[0]
+        t_quantity = toons_map[k]
+        t_gear = splitted_data[1]
+        t_zetas = splitted_data[2]
+        name_to_add = ""
+        if t_name != last_name:
+            last_name = t_name
+            name_to_add = t_name
+
+        name_list.append(name_to_add)
+        quantity_list.append(t_quantity)
+        gear_list.append(t_gear)
+        zetas_list.append(t_zetas)
+        
+    print(make_pretty_table(name_list,quantity_list,gear_list,zetas_list))
 
 if __name__ == "__main__":
 
